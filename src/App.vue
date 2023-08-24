ae2aa8275cfacc11b850b4152c450a4de0063451
beckynayere <rebecca.nayere@gmail.com>
2023-08-24 16:09:02 +0300

    update app vue

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import addItemForm from "./addItemForm";
+import listView from "./listView";
+
+export default {
+    components: {
+        addItemForm,
+        listView
+    },
+
+    data: function() {
+        return {
+            items: []
+        };
+    },
+    methods: {
+        getItems() {
+            axios
+                .get("api/items")
+                .then(res => {
+                    this.items = res.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    },
+    created() {
+        this.getItems();
+    }
+};
+</script>
+</style>
